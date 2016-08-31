80eb39c2a083c0fcfc6bd487e74e82ecea553f43
hueby <dennis.huebner89gmail.com>
2016-08-31 07:12:02 +0200

    I should not commit something in the morning..

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('My Module', function() {
+  it('should start array length 2', function() {
+    assert.equal(2, [1,2].length);
+  });
+});
